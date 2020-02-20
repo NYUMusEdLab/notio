71fe144619c936c9a9d12b654b5de32d6ef0c175
Marcus Gustafsson <marcus@marcus.ax>
2020-02-20 15:09:50 +0200

    Change notation of step 7 in Scale Steps

--- src/data/scalesObj.js
+++ src/data/scalesObj.js
@@ -6,18 +6,18 @@ const scales = [
   { 
     name: "Major (Ionian)",
     steps: [0,2,4,5,7,9,11,12],
-    numbers: ['1', '2', '3', '4', '5', '6', '7'],
+    numbers: ['1', '2', '3', '4', '5', '6', '△7'],
     major_seventh: 11
   },
   { 
     name: "Mixolydian",
     steps: [0,2,4,5,7,9,10,12],
-    numbers: ['1', '2', '3', '4', '5', '6', 'b7']
+    numbers: ['1', '2', '3', '4', '5', '6', '7']
   },
   { 
     name: "Lydian",
     steps: [0,2,4,6,7,9,11,12],
-    numbers: ['1', '2', '3', '#4', '5', '6', '7'],
+    numbers: ['1', '2', '3', '#4', '5', '6', '△7'],
     major_seventh: 11
   },
   {
@@ -28,44 +28,44 @@ const scales = [
   {
     name: "Minor Pentatonic",
     steps: [0,3,5,7,10,12],
-    numbers: ['1', 'b3', '4', '5', 'b7'],
+    numbers: ['1', 'b3', '4', '5', '7'],
   },
   { 
     name: "Minor (Natural/Aeolian)",
     steps: [0,2,3,5,7,8,10,12],
-    numbers: ['1', '2', 'b3', '4', '5', 'b6', 'b7']
+    numbers: ['1', '2', 'b3', '4', '5', 'b6', '7']
   },
   { 
     name: "Dorian",
     steps: [0,2,3,5,7,9,10,12],
-    numbers: ['1', '2', 'b3', '4', '5', '6', 'b7']
+    numbers: ['1', '2', 'b3', '4', '5', '6', '7']
   },
   { 
     name: "Harmonic Minor",
     steps: [0,2,3,5,7,8,11,12],
-    numbers: ['1', '2', 'b3', '4', '5', 'b6', '7'],
+    numbers: ['1', '2', 'b3', '4', '5', 'b6', '△7'],
     major_seventh: 11
   },
   { 
     name: "Melodic Minor",
     steps: [0,2,3,5,7,9,11,12],
-    numbers: ['1', '2', 'b3', '4', '5', '6', '7'],
+    numbers: ['1', '2', 'b3', '4', '5', '6', '△7'],
     major_seventh: 11
   },
   { 
     name: "Phrygian",
     steps: [0,1,3,5,7,8,10,12],
-    numbers: ['1', 'b2', 'b3', '4', '5', 'b6', 'b7']
+    numbers: ['1', 'b2', 'b3', '4', '5', 'b6', '7']
   },
   { 
     name: "Locrian",
     steps: [0,1,3,5,6,8,10,12],
-    numbers: ['1', 'b2', 'b3', '4', 'b5', 'b6', 'b7']
+    numbers: ['1', 'b2', 'b3', '4', 'b5', 'b6', '7']
   },
   {
     name: "Minor Blues",
     steps: [0,3,5,6,7,10,12],
-    numbers: ['1', 'b3', '4', '#4', '5', 'b7'],
+    numbers: ['1', 'b3', '4', '#4', '5', '7'],
   },
   {
     name: "Major Blues",
