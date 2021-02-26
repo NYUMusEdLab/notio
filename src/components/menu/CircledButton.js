a4fe6a3638a328daa73a3a70d182741de2e7b15c
Emilie <emi.zawa@gmail.com>
2021-02-26 17:23:38 +0100

    [Task #10] : drag n dropable modal

--- src/components/menu/CircledButton.js
+++ src/components/menu/CircledButton.js
@@ -1,28 +1,51 @@
+import React, { Component, createRef } from "react";
+import $ from "jquery";
+import gsap from "gsap";
+import Draggable from "gsap/Draggable";
 
-import React, { Component } from "react";
-import Popup from 'reactjs-popup';
 // import 'reactjs-popup/dist/index.css';
 
-import VideoSVG from '../../assets/img/Video';
-
+import VideoSVG from "../../assets/img/Video";
 
 const components = {
-  'video': <VideoSVG />,
-
+  video: <VideoSVG />,
 };
 class CircledButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    gsap.registerPlugin(Draggable);
+
+    $(".circledButton").click(function () {
+      $(".notio-popup").toggleClass("show");
+    });
+    Draggable.create(".notio-popup", {
+      type: "x,y",
+      edgeResistance: 0.65,
+      bounds: ".Piano",
+      inertia: true,
+    });
+  }
 
   render() {
-    return <div>
-      <Popup trigger={<div class="circledButton">
-        {components[this.props.label]}
-      </div>} position="bottom center">
-        <div><iframe id="ytplayer" type="text/html" width="300" height="168.75"
-          src="https://www.youtube.com/embed/?listType=playlist&list=PL7imp2jxKd0Bcx4cjR3gEMirkAzd84G_C&enablejsapi=1&loop=1&modestbranding=1&color=white"
-          frameborder="0" allowfullscreen></iframe></div>
-      </Popup>
-    </div>
-  };
+    return (
+      <div>
+        <div class="circledButton">{components[this.props.label]}</div>
+        <div class="notio-popup">
+          <iframe
+            id="ytplayer"
+            type="text/html"
+            width="300"
+            height="168.75"
+            src="https://www.youtube.com/embed/?listType=playlist&list=PL7imp2jxKd0Bcx4cjR3gEMirkAzd84G_C&enablejsapi=1&loop=1&modestbranding=1&color=white"
+            frameborder="0"
+            allowfullscreen
+          ></iframe>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default CircledButton;
