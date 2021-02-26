9a3271082ed7bda5754e5e142363fdf6ff1cee70
Emilie <emi.zawa@gmail.com>
2021-02-26 18:07:49 +0100

    [Task #10] : improve UI

--- src/components/menu/CircledButton.js
+++ src/components/menu/CircledButton.js
@@ -3,7 +3,7 @@ import $ from "jquery";
 import gsap from "gsap";
 import Draggable from "gsap/Draggable";
 
-// import 'reactjs-popup/dist/index.css';
+import ReactPlayer from "react-player";
 
 import VideoSVG from "../../assets/img/Video";
 
@@ -14,12 +14,14 @@ class CircledButton extends Component {
   constructor(props) {
     super(props);
   }
+
   componentDidMount() {
     gsap.registerPlugin(Draggable);
 
     $(".circledButton").click(function () {
       $(".notio-popup").toggleClass("show");
     });
+
     Draggable.create(".notio-popup", {
       type: "x,y",
       edgeResistance: 0.65,
@@ -33,15 +35,7 @@ class CircledButton extends Component {
       <div>
         <div class="circledButton">{components[this.props.label]}</div>
         <div class="notio-popup">
-          <iframe
-            id="ytplayer"
-            type="text/html"
-            width="300"
-            height="168.75"
-            src="https://www.youtube.com/embed/?listType=playlist&list=PL7imp2jxKd0Bcx4cjR3gEMirkAzd84G_C&enablejsapi=1&loop=1&modestbranding=1&color=white"
-            frameborder="0"
-            allowfullscreen
-          ></iframe>
+          <ReactPlayer url="https://youtube.com/playlist?list=PL7imp2jxKd0Bcx4cjR3gEMirkAzd84G_C" />
         </div>
       </div>
     );
