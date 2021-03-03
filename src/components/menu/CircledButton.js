4e5e08e8dd8dc569b2b5c80240d89327de87625b
Emilie <emi.zawa@gmail.com>
2021-03-03 13:23:47 +0100

    [Task #10] : create reusable Popup component

--- src/components/menu/CircledButton.js
+++ src/components/menu/CircledButton.js
@@ -3,18 +3,17 @@ import $ from "jquery";
 import gsap from "gsap";
 import Draggable from "gsap/Draggable";
 
-import ReactPlayer from 'react-player/lazy'
+import ReactPlayer from "react-player/lazy";
 
 import VideoSVG from "../../assets/img/Video";
 import UnderscoreSVG from "../../assets/img/Underscore";
 import CrossSVG from "../../assets/img/Cross";
-
+import Popup from "./Popup";
 
 const components = {
   video: <VideoSVG />,
 };
 class CircledButton extends Component {
-
   state = {
     url: null,
     playing: false,
@@ -22,68 +21,30 @@ class CircledButton extends Component {
     loaded: 0,
     duration: 0,
     minimized: false,
-    show: false
-  }
+    show: false,
+  };
   constructor(props) {
     super(props);
   }
 
-  componentDidMount() {
-    gsap.registerPlugin(Draggable);
-
-    Draggable.create(".notio-popup", {
-      type: "x,y",
-      edgeResistance: 0.65,
-      bounds: ".Piano",
-      inertia: true,
-    });
-  }
-
-  handleShow = () => {
-    this.setState({ show: !this.state.show })
-  }
-  
-  handlePlayPause = () => {
-    this.setState({ playing: !this.state.playing })
-  }
-
-  handleMinimize = () => {
-    this.setState({ minimized: !this.state.minimized })
-  }
-
-
-
   render() {
-
-    const { url, playing, played, loaded, duration, minimized, show} = this.state
+    const { url, playing } = this.state;
     return (
       <div>
-        <div
-          class="circledButton"
-          onClick={(e) => { this.handlePlayPause(); this.handleShow(); }}>
-            {components[this.props.label]}
-        </div>
-        <div
-          class={`notio-popup ${minimized ? "minimized" : ""} ${show ? "show" : ""}` }
-        >
-        <div class="notio-popup--header clearfix">
-          <div class="close notio-popup--button"
-            onClick={(e) => { this.handlePlayPause(); this.handleShow(); }}
-          ><CrossSVG /></div>
-          <div
-            class="minimize notio-popup--button"
-            onClick={this.handleMinimize}
-          ><UnderscoreSVG /></div>
-        </div>
-          <ReactPlayer 
-          ref={this.ref}
-          className='react-player'
-          url={url}
-          playing={playing}
-          width='100%'
-          height='100%'
-          url="https://youtube.com/playlist?list=PL7imp2jxKd0Bcx4cjR3gEMirkAzd84G_C" />
-        </div>
+        <Popup
+          picto={components[this.props.label]}
+          content={
+            <ReactPlayer
+              ref={this.ref}
+              className="react-player"
+              url={url}
+              playing={playing}
+              width="100%"
+              height="100%"
+              url="https://youtube.com/playlist?list=PL7imp2jxKd0Bcx4cjR3gEMirkAzd84G_C"
+            />
+          }
+        />
       </div>
     );
   }
---
+++ src/components/menu/Popup.js
@@ -0,0 +1,105 @@
+import React, { Component, createRef } from "react";
+import $ from "jquery";
+import gsap from "gsap";
+import Draggable from "gsap/Draggable";
+
+import ReactPlayer from "react-player/lazy";
+
+import VideoSVG from "../../assets/img/Video";
+import UnderscoreSVG from "../../assets/img/Underscore";
+import CrossSVG from "../../assets/img/Cross";
+
+const components = {
+  video: <VideoSVG />,
+};
+class Popup extends Component {
+  state = {
+    minimized: false,
+    show: false,
+  };
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    gsap.registerPlugin(Draggable);
+
+    // $('.notio-popup').each(function() {
+    //   gsap.set('.resize-handle', { top: drag.width(), left: drag.height() });
+    // }
+    // gsap.set(".resize-handle", { top: $('.notio-popup').width(), left: $('.notio-popup').height() });
+
+    Draggable.create(".notio-popup", {
+      type: "x,y",
+      edgeResistance: 0.65,
+      bounds: ".Piano",
+      inertia: true,
+    });
+
+    // Draggable.create(".resize-handle", {
+    //   type:"bottom,right",
+    //   onPress: function(e) {
+    //     e.stopPropagation(); // cancel drag
+    //   },
+    //   onDrag: function(e) {
+    //     gsap.set(this.target.parentNode, { width: this.x, height: this.y });
+    //   }
+    // });
+  }
+
+  handleShow = () => {
+    this.setState({ show: !this.state.show });
+  };
+
+  handlePlayPause = () => {
+    this.setState({ playing: !this.state.playing });
+  };
+
+  handleMinimize = () => {
+    this.setState({ minimized: !this.state.minimized });
+  };
+
+  render() {
+    const { minimized, show } = this.state;
+    return (
+      <div>
+        <div
+          class="circledButton"
+          onClick={(e) => {
+            this.handlePlayPause();
+            this.handleShow();
+          }}
+        >
+          {this.props.picto}
+        </div>
+        <div
+          class={`notio-popup ${minimized ? "minimized" : ""} ${
+            show ? "show" : ""
+          }`}
+        >
+          <div class="notio-popup--header clearfix">
+            <div
+              class="close notio-popup--button"
+              onClick={(e) => {
+                this.handlePlayPause();
+                this.handleShow();
+              }}
+            >
+              <CrossSVG />
+            </div>
+            <div
+              class="minimize notio-popup--button"
+              onClick={this.handleMinimize}
+            >
+              <UnderscoreSVG />
+            </div>
+          </div>
+          {this.props.content}
+          {/* <div class='resize-handle'></div> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Popup;
