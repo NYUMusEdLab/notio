289a5a4c209894bbd972b9e95d806ce5e281f412
MartinBruun <mbm93fen@gmail.com>
2021-08-16 12:43:56 +0200

    Basic setup before installing babel dependencies

--- src/__test__/WholeApp.unit.test.js
+++ src/__test__/WholeApp.unit.test.js
@@ -1,5 +1,6 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import WholeApp from "../WholeApp";
 
 test("Hello", () => {
     expect(1).toBe(1);
