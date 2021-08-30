d1e62cf35a5c7b0f48fa19adeeca148d2df7199e
saxjax <saxjax@saxjax.dk>
2021-08-31 00:10:55 +0200

    changed DropdownCustomScaleMenu into a react component

--- src/components/menu/DropdownCustomScaleMenu.js
+++ src/components/menu/DropdownCustomScaleMenu.js
@@ -12,37 +12,42 @@ import CustomScaleImg from "../../assets/img/CustomScale";
 
 // import Checkbox from "../form/Checkbox";
 
-export const DropdownCustomScaleMenu = (props ) => (
-  <div className="menu">
-    <Popup
-      trigger={<div className="sub-menu"> - Customise -</div>}
-      position="left top"
-      on="hover"
-      closeOnDocumentClick
-      mouseLeaveDelay={300}
-      mouseEnterDelay={0}
-      contentStyle={{ padding: '2px', border: 'none' }}
-      arrow={false}
-      
-    >
-       <div className="sub-menu">
-          <div className="navbar-item menu-custom-scale" >
-            <SubMenu
-              active={true}
-              title="CustomScale"
-              selected={"CustomScale"}
-              selectedImg= <CustomScaleImg />
-              content={
-                <CustomScaleSelector //TODO: add initoptions for custom scale, matching current scale, add function handleCustomScale
-                  initOptions={props.state.scaleObject} //TODO: fix to customscale creation
-                  handleChange={props.handleChangeCustomScale} //TODO: fix this function, it should modifi the customScale in WholeApp
-                />
-              }
-            />
-            
+export class DropdownCustomScaleMenu extends Component {
+  
+  render() {
+    return(
+    <div className="menu">
+      <Popup
+        trigger={<div className="sub-menu"> - Customise -</div>}
+        position="left top"
+        on="hover"
+        closeOnDocumentClick
+        mouseLeaveDelay={300}
+        mouseEnterDelay={0}
+        contentStyle={{ padding: '2px', border: 'none' }}
+        arrow={false}
+        
+      >
+        <div className="sub-menu">
+            <div className="navbar-item menu-custom-scale" >
+              <SubMenu
+                active={true}
+                title="CustomScale"
+                selected={"CustomScale"}
+                selectedImg= <CustomScaleImg />
+                content={
+                  <CustomScaleSelector //TODO: add initoptions for custom scale, matching current scale, add function handleCustomScale
+                    initOptions={this.props.state.scaleObject} //TODO: fix to customscale creation
+                    handleChange={this.props.handleChangeCustomScale} //TODO: fix this function, it should modifi the customScale in WholeApp
+                  />
+                }
+              />
+              
+            </div>
           </div>
-        </div>
-    </Popup>
-    < div> ____________ </div>
-  </div>
-);
+      </Popup>
+      < div> ____________ </div>
+    </div>
+  );
+  }
+}
