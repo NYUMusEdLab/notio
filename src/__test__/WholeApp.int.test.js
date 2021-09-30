c6a8a3de5cee79220d4f373cb2d69d3c152e9375
MartinBruun <mbm93fen@gmail.com>
2021-09-30 21:09:10 +0200

    Update tests, but render is apparently not supported

--- src/__test__/WholeApp.int.test.js
+++ src/__test__/WholeApp.int.test.js
@@ -1,6 +1,6 @@
 import * as React from 'react'; // Necessary to run the tests, apparently.
 import { MemoryRouter, Route } from 'react-router-dom';
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Piano } from "@tonejs/piano";
 import WholeApp from "../WholeApp";
@@ -11,46 +11,40 @@ import WholeApp from "../WholeApp";
     (And later on tests integration between plugins and the TopMenu and/or KeyBoard)
 */
 
-// If possible, parametrize such as to test different configurations of WholeApp
-test("Octave plus button in Sound menu should increase octave on keyboard", () => {
-    render(
-        <MemoryRouter>
-            <Route exact path="/" component={WholeApp}></Route>;
-        </MemoryRouter>
-    );
-    const sound_menu = screen.getByText("Sound");
-    userEvent.click(sound_menu);
-    const octave_in_sound_menu = screen.getByText("Octave:",{exact:false})
-    expect(octave_in_sound_menu.textContent).toBe("Octave: 4")
+describe("Sound menu in the TopMenu to", () =>{
+    test("Octave plus button in Sound menu should increase octave", () => {
+        render(
+            <MemoryRouter>
+                <Route exact path="/" component={WholeApp}></Route>;
+            </MemoryRouter>
+        );
+        const sound_menu = screen.getByText("Sound");
+        userEvent.click(sound_menu);
+        const octave_in_sound_menu = screen.getByText("Octave:",{exact:false})
+        expect(octave_in_sound_menu.textContent).toBe("Octave: 4")
     
-    const plus_button = screen.getByText("+");
-    userEvent.click(plus_button);
+        const plus_button = screen.getByText("+");
+        userEvent.click(plus_button);
 
-    expect(octave_in_sound_menu.textContent).toBe("Octave: 5")
-    // Assert that the SoundMaker module receives the proper octave when playing a note
-})
+        expect(octave_in_sound_menu.textContent).toBe("Octave: 5")
+        // Assert that the SoundMaker module receives the proper octave when playing a note
+    })
 
-// If possible, parametrize such as to test different configurations of WholeApp
-
-/*
-Currently getting error that using render two times in a file is "not supported by jest".
-Will be fixed in a later PR, but cant take care of it now sadly.
-
-test("Octave minus button in Sound menu should decrease octave on keyboard", () => {
-    render(
-        <MemoryRouter>
-            <Route exact path="/" component={WholeApp}></Route>;
-        </MemoryRouter>
-    );
-    const sound_menu = screen.getByText("Sound");
-    userEvent.click(sound_menu);
-    const octave_in_sound_menu = screen.getByText("Octave:",{exact:false})
-    expect(octave_in_sound_menu.textContent).toBe("Octave: 4")
+    test("Octave minus button in Sound menu should decrease octave on keyboard", () => {
+        render(
+            <MemoryRouter>
+                <Route exact path={"/"} component={WholeApp}></Route>;
+            </MemoryRouter>
+        );
+        const sound_menu = screen.getByText("Sound");
+        userEvent.click(sound_menu);
+        const octave_in_sound_menu = screen.getByText("Octave:",{exact:false})
+        expect(octave_in_sound_menu.textContent).toBe("Octave: 4")
     
-    const plus_button = screen.getByText("-");
-    userEvent.click(plus_button);
+        const plus_button = screen.getByText("-");
+        userEvent.click(plus_button);
 
-    expect(octave_in_sound_menu.textContent).toBe("Octave: 3")
-    // Assert that the SoundMaker module receives the proper octave when playing a note
+        expect(octave_in_sound_menu.textContent).toBe("Octave: 3")
+        // Assert that the SoundMaker module receives the proper octave when playing a note
+    })
 })
-*/
