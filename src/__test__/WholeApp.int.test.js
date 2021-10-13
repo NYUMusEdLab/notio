d0ab8e7e307144e7b3988a5ff86547b3150a2505
MartinBruun <mbm93fen@gmail.com>
2021-10-13 19:22:49 +0200

    Include initial entries to memoryRouter

--- src/__test__/WholeApp.int.test.js
+++ src/__test__/WholeApp.int.test.js
@@ -1,10 +1,11 @@
 import * as React from 'react'; // Necessary to run the tests, apparently.
 import { MemoryRouter, Route } from 'react-router-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import SoundMaker from "../components/SoundMaker";
 import ReactPlayer from "react-player/lazy";
 import WholeApp from "../WholeApp";
+import { Time } from 'tone';
 
 /*
     File containing all integration tests of the WholeApp module.
@@ -22,25 +23,29 @@ beforeEach(() => {
 })
 
 describe("Root menu in the TopMenu to", () =>{
-    test("Octave plus button should increase octave", async () => {
+    test.each([
+        ["C", 4, ["/"]],
+        ["H", 3, ["/shared/INyllzBj7efsVe54qtFl"]]
+    ])("Octave plus button should increase octave", async (root_note, octave, url) => {
         expect(SoundMaker).not.toHaveBeenCalled();
-        render(
-            <MemoryRouter>
-                <Route exact path="/" component={WholeApp}></Route>;
+        await waitFor(() => render(
+            <MemoryRouter initialEntries ={url}>
+                <Route path="/shared/:sessionId" component={WholeApp} />
+                <Route exact path={"/"} component={WholeApp}></Route>;
             </MemoryRouter>
-        );
+        ));
         const octave_in_menu = screen.getByText("Octave:",{exact:false});
-        expect(octave_in_menu.textContent).toBe("Octave: 4");
+        expect(octave_in_menu.textContent).toBe("Octave: "+octave);
     
         const plus_button = screen.getByText("+");
         userEvent.click(plus_button);
 
-        expect(octave_in_menu.textContent).toBe("Octave: 5");
+        expect(octave_in_menu.textContent).toBe("Octave: "+(octave+1));
         expect(SoundMaker).toHaveBeenCalledTimes(1);
-        const root_key = screen.getByTestId("ColorKey:C5");
+        const root_key = screen.getByTestId("ColorKey:"+root_note+(octave+1));
         userEvent.click(root_key);
-        expect(SoundMaker.mock.instances[0].startSound).toHaveBeenCalledWith("C5");
-        expect(SoundMaker.mock.instances[0].stopSound).toHaveBeenCalledWith("C5");
+        expect(SoundMaker.mock.instances[0].startSound).toHaveBeenCalledWith(root_note+(octave+1));
+        expect(SoundMaker.mock.instances[0].stopSound).toHaveBeenCalledWith(root_note+(octave+1));
     })
 
     test("Octave minus button should decrease octave", () => {
