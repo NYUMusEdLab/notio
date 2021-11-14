7235a77e8135e043e3038b38151ac21c6a7256e1
saxjax <saxjax@saxjax.dk>
2021-11-14 16:17:53 +0100

    added colorblind schemas to keys 2 and 3, and standard to 1

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -9,10 +9,12 @@ import MusicScale from "../Model/MusicScale";
 //import colors from "../data/colors";
 
 const colorsD = {
-  pastel:["#F8BBD0", "#E1BEE7", "#D1C4E9", "#C5CAE9", "#BBDEFB", "#B2EBF2", "#B2DFDB", "#C8E6C9", "#DCEDC8", "#FFF9C4", "#FFECB3", "#FFE0B2"],
-  greenis:["#FFAAAA", "#CCFFFF", "#FFFFCC", "#99CCCC", "#66CC99", "#9999CC", "#CC6699", "#FF9900", "#CC99CC", "#FFCC99", "#CCCCFF", "#CCCCCC"],
-  bright:["#ff0000", "#ff8c00", "#ffff00", "#c0c0c0", "#ffffff", "#228b22", "#00ff7f", "#00ffff", "#0000ff", "#87cefa", "#8a2be2", "#ee82ee"],
-  other:["#cd0223", "#d45331", "#e39255", "#ecbb10", "#e3d98a", "#47e643", "#28cbb9", "#049496", "#2f7ecc", "#674ed8", "#a059ed", "#ba04ff", "#ba05a5"]
+  colorBlind1:["#882255", "#AA4499", "#CC6677", "#DDCC77", "#88CCEE", "#44AA99", "#117733","#4035FF", "#A8B8EF", "#332288", "#9BB34D", "#D3F3B8"],
+  colorBlind2:["#920000", "#924900", "#db6d00", "#24ff24", "#ffff6d","#ff6db6", "#ffb6db", "#490092", "#006ddb", "#b66dff","#6db6ff", "#b6dbff"],//"#009292"
+  pastel:     ["#F8BBD0", "#E1BEE7", "#D1C4E9", "#C5CAE9", "#BBDEFB", "#B2EBF2", "#B2DFDB", "#C8E6C9", "#DCEDC8", "#FFF9C4", "#FFECB3", "#FFE0B2"],
+  greenis:    ["#FFAAAA", "#CCFFFF", "#FFFFCC", "#99CCCC", "#66CC99", "#9999CC", "#CC6699", "#FF9900", "#CC99CC", "#FFCC99", "#CCCCFF", "#CCCCCC"],
+  bright:     ["#ff0000", "#ff8c00", "#ffff00", "#c0c0c0", "#ffffff", "#228b22", "#00ff7f", "#00ffff", "#0000ff", "#87cefa", "#8a2be2", "#ee82ee"],
+  other:      ["#cd0223", "#d45331", "#e39255", "#ecbb10", "#e3d98a", "#47e643", "#28cbb9", "#049496", "#2f7ecc", "#674ed8", "#a059ed", "#ba04ff"]
 };
 // Using 'code' property for compatibility with AZERTY, QWERTY... keyboards
 const keycodes = [
@@ -108,16 +110,22 @@ class Keyboard extends Component {
     const activeKeyCodes = extendedKeyboard ? keycodesExtended : keycodes;
     const mapKeyDown = activeKeyCodes.indexOf(e.code);
     if (e.code === "Digit1") {
-      this.setState({colorname:"pastel"})
+      this.setState({ colorname: "standard" });
     }
     if (e.code === "Digit2") {
-      this.setState({colorname:"greenis"})
+      this.setState({ colorname: "colorBlind1" });
     }
     if (e.code === "Digit3") {
-      this.setState({colorname:"bright"})
+      this.setState({ colorname: "colorBlind2" });
     }
     if (e.code === "Digit4") {
-      this.setState({colorname:"other"})
+      this.setState({ colorname: "other" });
+    }
+    if (e.code === "Digit5") {
+      this.setState({ colorname: "greenis" });
+    }
+    if (e.code === "Digit6") {
+      this.setState({ colorname: "bright" });
     }
 
     if (
