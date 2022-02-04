945f776bbc69e1ee9a96212b4d2f9bc220026743
saxjax <saxjax@saxjax.dk>
2022-02-04 18:00:59 +0100

    now the video scales, but every other element from the video is not working

--- src/components/menu/Overlay.js
+++ src/components/menu/Overlay.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
  const Overlay = (props) => {
-    const content = <aside className="overlay">{<div className="overlay content">{props.children}</div>}</aside>;
+    const content = <aside className="overlay">{<div className="content">{props.children}</div>}</aside>;
     return ReactDOM.createPortal(content, document.getElementById('overlay_root'))
 
 }
