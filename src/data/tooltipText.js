8f9048680efa8a533588f6f8f6c811cedf774dc7
MartinBruun <mbm93fen@gmail.com>
2022-03-08 11:38:02 +0100

    Update tooltips

--- src/data/tooltipText.js
+++ src/data/tooltipText.js
@@ -1,14 +1,14 @@
 const tooltipText = {
-    "showKeyboard": "Show Keyboard <br/> Toggles the keyboard on/off at the bottom of the screen",
-    "extendKeyboard": "Extend Keyboard <br/> Toggles the keyboard between only showing 1 octave and the 3 extra notes surrounding the octave on top and bottom <br/>",
+    "showKeyboard": "Show Keyboard <br/> Toggles the piano keyboard on/off at the bottom of the screen",
+    "extendKeyboard": "Extend Keyboard <br/> Toggles the ambitus of the keyboard, from 1 octave to more octaves <br/>",
     "sound": "Sound <br/> Chooses what sound the keyboard should make <br/> Presently only supports a piano sound",
     "notation": "Notation <br/> Chooses the type of notations you want to be shown",
-    "root": "Root <br/> Chooses what the root note is, and what octave it is in",
+    "root": "Root <br/> Chooses the root note of the scale, and what octave it is in",
     "scale": "Scale <br/> Chooses the scale being used on the keyboard <br/> Only notes in the scale are possible to play on <br/> It is possible to create a custom scale",
     "clefs": "Clefs <br/> Chooses what clef will be used when showing the notes <br/> It is also possible to turn off showing the notes",
     "shareThisSetup": "Share This Setup <br/> Creates a url you can share, so that others receive an identical setup to the one you currently have",
     "videoPlayer": "Video Player <br/> Shows a popup with a Youtube video player <br/> You can play any video by copy-pasting the url into the Customize tab <br/> The popup can also be moved and resized by dragging it",
-    "keyboard": "Keyboard <br/> The Keyboard can be played by using the mouse <br/> The Keyboard also support play using both right and left hand <br/> The right hand starts at F and goes to the right<br/> The left hand uses ZXC ASD QWE and can be transposed up and down with the arrow keys <br/> In extended keyboard mode, you can't use both right and left hand, but instead play from A and up. F is still the root note<br/>The colouring of the keyboard can be changed on the 1,2,3,4 and 5 keys",
+    "keyboard": "Keyboard <br/> The Keyboard can be played by using the mouse <br/> The Keyboard also support play using both right and left hand <br/> The right hand starts at F and goes to the right<br/> The left hand uses ZXC ASD QWE and can be transposed up and down with the arrow keys <br/> In extended keyboard mode, you can't use both right and left hand, but instead play from A and up. F is still the root note <br/>The colouring of the keyboard can be changed on the 1,2,3,4 and 5 keys",
     "help": "? Button <br/> Toggles these tooltips on and off"
 }
 
