939c10483b3126e5bd099f6ef44029fb51c32373
saxjax <saxjax@saxjax.dk>
2022-03-13 11:40:51 +0100

    video now updates when a submiturl button is pressed,changed Videotutorial2 to functionalcomponent, added ref hooks on Form.Control, this may not be necessary added local state handling to handle url change

--- src/components/menu/Overlay.js
+++ src/components/menu/Overlay.js
@@ -12,13 +12,13 @@ export default class Overlay extends Component {
     draggable: false,
   };
 
-  handleClick = (event) => {
-    const draggable = !this.state.draggable;
-    const className = draggable ? "overlay drag" : "overlay nodrag";
+  // handleClick = (event) => {
+  //   const draggable = !this.state.draggable;
+  //   const className = draggable ? "overlay drag" : "overlay nodrag";
 
-    this.setState({ draggable: draggable });
-    this.setState({ classname: className });
-  };
+  //   this.setState({ draggable: draggable });
+  //   this.setState({ classname: className });
+  // };
 
   content = (
     <aside className="overlay">
@@ -34,14 +34,12 @@ export default class Overlay extends Component {
   topBar() {
     return (
       <div className="navbar clearfix drag">
-        {/* Toggle Piano */}
         {/* <Toggle
           className="navbar-item__toggle"
           title="Show keyboard"
           // onChange={}
           // checked={}
         /> */}
-        {/* <div className="notio-popup--header clearfix"> */}
         <div
           className="navbar-item__close"
           onClick={(e) => {
@@ -63,22 +61,8 @@ export default class Overlay extends Component {
   }
 
   render() {
-    // const { classname } = this.state;
-    // return ReactDOM.createPortal(<Draggable handle={'.drag'}><Button onClick={this.handleClick} className={this.state.classname} title={this.state.classname}>{this.state.classname+this.state.draggable}</Button></Draggable>, document.getElementById('overlay_root'))
-
     return ReactDOM.createPortal(
       <Draggable handle={".drag"}>
-        {/* <div className="notio-popup--header clearfix"> */}
-        {/* <div
-          className="close notio-popup--button"
-          onClick={(e) => {
-            this.props.onClickCloseHandler();
-            // this.handleShow();
-          }}>
-          <CrossSVG />
-        </div> */}
-
-        {/* </div> */}
         <div
           // onClick={this.handleClick}
           className={this.state.classname}
@@ -90,31 +74,3 @@ export default class Overlay extends Component {
     );
   }
 }
-//  handleMousePosition(event){
-//      const temp_pos = {x:event.clientX, y:event.clientY}
-//   //    this.saveCursorPosition(temp_pos.x,temp_pos.y)
-//      document.documentElement.style.setProperty('--x', temp_pos.x + 'px');
-//      document.documentElement.style.setProperty('--y', temp_pos.y + 'px');
-
-//   //    console.log(window.innerWidth)
-
-//  }
-
-//  handleDragStopped(event){
-//   this.setState({pos : {x:event.clientX, y:event.clientY}})
-//  }
-
-//   saveCursorPosition = function(x, y) {
-//      this.pos.x = (x / window.innerWidth).toFixed(2);
-//      this.pos.y = (y / window.innerHeight).toFixed(2);
-//   //    document.documentElement.style.setProperty('--x', this.pos.x);
-//   //    document.documentElement.style.setProperty('--y', this.pos.y);
-//  }
-
-//  const Overlay = (props) => {
-//     const content = <aside className="overlay">{<div className="content">{props.children}</div>}</aside>;
-//     return ReactDOM.createPortal(content, document.getElementById('overlay_root'))
-
-// }
-
-// export default Overlay;
--- src/components/menu/VideoTutorial2.js
+++ src/components/menu/VideoTutorial2.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useRef, useState } from "react";
 import ReactPlayer from "react-player/lazy";
 import { Tabs, Tab, Form, Button } from "react-bootstrap";
 // import VideoSVG from "../../assets/img/Video";
@@ -9,103 +9,107 @@ import Overlay from "./Overlay";
 //   video: <VideoSVG />,
 // };
 
-class VideoTutorial extends Component {
-  state = {
-    playing: false,
-    played: 0,
-    loaded: 0,
-    duration: 0,
-    minimized: false,
-    show: this.props.vissible ? true : false,
-    activeTab: "playlist",
-    playerIsReady: false,
+const VideoTutorial = (props) => {
+  const urlInputRef = useRef();
+
+  const [playing, setPlaying] = useState(false);
+  const [played, setPlayed] = useState(0);
+  const [loaded, setLoaded] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [minimized, setMinimized] = useState(false);
+  const [show, setShow] = useState(props.vissible);
+  const [activeTab, setActiveTab] = useState("playlist");
+  const [playerIsReady, setPlayerIsReady] = useState(false);
+  const [videoUrl, setVideoUrl] = useState(props.videoUrl);
+
+  const handlePlayPause = () => {
+    setPlaying(!playing);
+    // this.setState({ playing: !this.state.playing });
+    props.handleChangeVideoVisibility();
   };
 
-  handlePlayPause = () => {
-    this.setState({ playing: !this.state.playing });
-    this.props.handleChangeVideoVisibility();
-  };
-
-  handleSubmit = (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    // set video url
-    console.log(event.target.elements[0].value);
-    this.props.handleChangeVideoUrl(event.target.elements[0].value);
-
-    this.setState({
-      activeTab: "playlist",
-    });
+    setVideoUrl(event.target.elements[0].value);
+    props.handleChangeVideoUrl(event.target.elements[0].value);
+    setActiveTab("playlist");
   };
 
-  handleSelect = (key) => {
+  const handleSelect = (key) => {
     // A bit dummy but need to control tabs after submit (cf handleSumbit())
-    if (key === "playlist") this.setState({ activeTab: "playlist" });
-    if (key === "change_video") this.setState({ activeTab: "change_video" });
+
+    if (key === "playlist") {
+      setActiveTab("playlist");
+      // this.setState({ activeTab: "playlist" });
+    }
+    if (key === "change_video") {
+      setActiveTab("change_video");
+      //   this.setState({ activeTab: "change_video" });
+    }
   };
 
-  playerOnReady = (event) => {
+  const playerOnReady = (event) => {
     // A bit dummy but need to control tabs after submit (cf handleSumbit())
-    this.setState({ playerIsReady: true });
+    setPlayerIsReady(false);
+    setPlaying(false);
+    setPlayerIsReady(true);
+
+    // this.setState({ playerIsReady: true });
   };
 
-  resetVideoUrl = (event) => {
-    this.props.resetVideoUrl();
-    this.setState({
-      activeTab: "playlist",
-    });
+  const resetVideoUrl = (event) => {
+    props.resetVideoUrl();
+    setVideoUrl(props.videoUrl);
+    setActiveTab("playlist");
+    // setState({
+    //   activeTab: "playlist",
+    // });
   };
 
-  render() {
-    // const { playing, activeTab } = this.state;
+  // const { playing, activeTab } = state;
+  return (
+    <React.Fragment>
+      <Overlay visible={show} key={videoUrl}>
+        <div className="tabs-wrapper">
+          {/* <Tabs defaultActiveKey="playlist" activeKey={state.activeTab} onSelect={handleSelect}  id="controlled-tab-example"> */}
+          <Tabs defaultActiveKey="playlist" activeTab={activeTab} id="controlled-tab-example">
+            <Tab eventKey="playlist" title="Playlist">
+              <ReactPlayer
+                className="react-player"
+                playing={playing}
+                width="100%"
+                height="100%"
+                url={videoUrl}
+                controls={true}
+                onReady={playerOnReady}
+              />
+            </Tab>
+            <Tab eventKey="change_video" title="Customize">
+              <div>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group controlId="formYoutubeUrl">
+                    <Form.Label>Video url</Form.Label>
+                    <Form.Control type="text" placeholder={videoUrl} ref={urlInputRef} />
+                    <Form.Text className="text-muted">
+                      Enter the url for any video that you want to use with the app
+                      {videoUrl}
+                    </Form.Text>
 
-    return (
-      <React.Fragment>
-        <Overlay visible={this.props.vissible}>
-          <div className="tabs-wrapper">
-            {/* <Tabs defaultActiveKey="playlist" activeKey={this.state.activeTab} onSelect={this.handleSelect}  id="controlled-tab-example"> */}
-            <Tabs
-              defaultActiveKey="playlist"
-              activeTab={this.state.activeTab}
-              id="controlled-tab-example">
-              <Tab eventKey="playlist" title="Playlist">
-                <ReactPlayer
-                  // key={"https://www.youtube.com/watch?v=" + this.props.videoUrl}
-                  ref={this.ref}
-                  className="react-player"
-                  playing={this.state.playing}
-                  width="100%"
-                  height="100%"
-                  url={this.props.videoUrl}
-                  controls={true}
-                  // onReady={this.playerOnReady}
-                />
-              </Tab>
-              <Tab eventKey="change_video" title="Customize">
-                <div>
-                  <Form onSubmit={this.handleSubmit}>
-                    <Form.Group controlId="formYoutubeUrl">
-                      <Form.Label>Video url</Form.Label>
-                      <Form.Control type="text" placeholder="Enter url" />
-                      <Form.Text className="text-muted">
-                        Enter the url for any video that you want to use with the app
-                      </Form.Text>
-
-                      <Button variant="primary" type="submit">
-                        Use this video
-                      </Button>
-                      <Button variant="outline-danger" onClick={this.resetVideoUrl}>
-                        Reset to Notio Tutorial
-                      </Button>
-                    </Form.Group>
-                  </Form>
-                </div>
-              </Tab>
-            </Tabs>
-          </div>
-        </Overlay>
-      </React.Fragment>
-    );
-  }
-}
+                    <Button variant="primary" type="submit">
+                      Use this video
+                    </Button>
+                    <Button variant="outline-danger" onClick={resetVideoUrl}>
+                      Reset to Notio Tutorial
+                    </Button>
+                  </Form.Group>
+                </Form>
+              </div>
+            </Tab>
+          </Tabs>
+        </div>
+      </Overlay>
+    </React.Fragment>
+  );
+};
 
 export default VideoTutorial;
