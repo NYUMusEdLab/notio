6bd8aa45cea000a0472116508cf0bb571d736d81
saxjax <saxjax@saxjax.dk>
2022-06-13 11:47:01 +0200

    quick fix on customScale popup position

--- src/components/menu/DropdownCustomScaleMenu.js
+++ src/components/menu/DropdownCustomScaleMenu.js
@@ -5,35 +5,33 @@ import Popup from "reactjs-popup";
 import CustomScaleSelector from "./CustomScaleSelector";
 import SubMenu from "./SubMenu";
 import CustomScaleImg from "../../assets/img/CustomScale";
+import Overlay from "./Overlay";
 
 export class DropdownCustomScaleMenu extends Component {
-  
   render() {
-    return(
-    <div 
-    className={this.props.menuTextClassName}>
-      <Popup
-        trigger={<div className="label-wrapper">Customise</div>}
-        position="left top"
-        on="hover"
-        repositionOnResize = {true}
-        offsetY={-300}
-        closeOnEscape ={true}
-        closeOnDocumentClick
-        mouseLeaveDelay={300}
-        mouseEnterDelay={0}
-        contentStyle={{ padding: '2px', border: 'none' }}
-        arrow={false}
-        
-      >
-        <div className="sub-menu">
-            <div className="navbar-item menu-custom-scale" >
+    return (
+      <div className={this.props.menuTextClassName}>
+        <Popup
+          trigger={<div className="label-wrapper">Customise</div>}
+          position="left top"
+          on="hover"
+          repositionOnResize={true}
+          offsetY={-600}
+          closeOnEscape={true}
+          closeOnDocumentClick
+          mouseLeaveDelay={300}
+          mouseEnterDelay={0}
+          contentStyle={{ padding: "2px", border: "none" }}
+          arrow={false}>
+          <div className="sub-menu">
+            <div className="navbar-item menu-custom-scale">
+              <Overlay></Overlay>
               <SubMenu
-                className = {this.props.menuTextClassName}
+                className={this.props.menuTextClassName}
                 active={true}
                 title=""
                 selected={"CustomScale"}
-                selectedImg= {<CustomScaleImg />}
+                selectedImg={<CustomScaleImg />}
                 content={
                   <CustomScaleSelector //TODO: add initoptions for custom scale, matching current scale, add function handleCustomScale
                     initOptions={this.props.state.scaleObject} //TODO: fix to customscale creation
@@ -41,11 +39,10 @@ export class DropdownCustomScaleMenu extends Component {
                   />
                 }
               />
-              
             </div>
           </div>
-      </Popup>
-    </div>
-  );
+        </Popup>
+      </div>
+    );
   }
 }
