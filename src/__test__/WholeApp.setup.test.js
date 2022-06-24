552c1392db57881891dc32be1beed097a1261ecf
saxjax <saxjax@saxjax.dk>
2022-06-24 20:47:57 +0200

    imported jest to all test files

--- src/__test__/WholeApp.setup.test.js
+++ src/__test__/WholeApp.setup.test.js
@@ -1,9 +1,10 @@
-import * as React from 'react'; // Necessary to run the tests, apparently.
-import { MemoryRouter, Route } from 'react-router-dom';
-import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import * as React from "react"; // Necessary to run the tests, apparently.
+import { MemoryRouter, Route } from "react-router-dom";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SoundMaker from "../components/SoundMaker";
 import WholeApp from "../WholeApp";
+import "jest";
 
 /*
     ERASE THIS TEXT AND WRITE WHAT FEATURE THIS FILE TESTS
@@ -15,36 +16,36 @@ import WholeApp from "../WholeApp";
 
 // This is necessary to make waitFor works, which makes sure Notio renders /shared/urls, otherwise its a loading screen.
 // To make sure it loads, the "await waitFor(() => screen.getAllByText("Root"));" is added after the render.
-import MutationObserver from 'mutation-observer'
-global.MutationObserver = MutationObserver 
+import MutationObserver from "mutation-observer";
+global.MutationObserver = MutationObserver;
 
 jest.mock("../components/SoundMaker"); // Automatic mock, which can be asserted against
 jest.mock("react-player/lazy"); // Manual mock in __mock__ folder, made just to make the tests pass
 
 beforeEach(() => {
-    SoundMaker.mockClear();
-})
+  SoundMaker.mockClear();
+});
 
-describe("THIS IS THE FEATURE BEING TESTED", () =>{
-    test.each([
-        [["/"]],
-        [["/shared/INyllzBj7efsVe54qtFl"]]
-    ])("THIS IS A SPECIFIC USE OF THE FEATURE TESTED IN DIFFERENT CONFIGURATIONS (url)", async (url) => {
-        // Arrange the server, wait for the page to be loaded and make sure the SoundMaker module is working
-        expect(SoundMaker).not.toHaveBeenCalled();
-        render(
-            <MemoryRouter initialEntries ={url}>
-                <Route path="/shared/:sessionId" component={WholeApp} />
-                <Route exact path={"/"} component={WholeApp}></Route>;
-            </MemoryRouter>
-        );
-        await waitFor(() => screen.getAllByText("Root"));
-        expect(SoundMaker).toHaveBeenCalledTimes(1);
+describe("THIS IS THE FEATURE BEING TESTED", () => {
+  test.each([[["/"]], [["/shared/INyllzBj7efsVe54qtFl"]]])(
+    "THIS IS A SPECIFIC USE OF THE FEATURE TESTED IN DIFFERENT CONFIGURATIONS (url)",
+    async (url) => {
+      // Arrange the server, wait for the page to be loaded and make sure the SoundMaker module is working
+      expect(SoundMaker).not.toHaveBeenCalled();
+      render(
+        <MemoryRouter initialEntries={url}>
+          <Route path="/shared/:sessionId" component={WholeApp} />
+          <Route exact path={"/"} component={WholeApp}></Route>;
+        </MemoryRouter>
+      );
+      await waitFor(() => screen.getAllByText("Root"));
+      expect(SoundMaker).toHaveBeenCalledTimes(1);
 
-        // (Extra) Arrange
+      // (Extra) Arrange
 
-        // Act
+      // Act
 
-        // Assert
-    })
-})
+      // Assert
+    }
+  );
+});
