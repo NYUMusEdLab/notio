6dfd085165c2acb63029185ef93f0af1ab1ab35b
saxjax <saxjax@saxjax.dk>
2022-07-04 23:47:40 +0200

    default value to Checkbox

--- src/components/form/Checkbox.js
+++ src/components/form/Checkbox.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Checkbox = ({ className, label, isSelected, onCheckboxChange }) => (
+const Checkbox = ({ className = "", label, isSelected, onCheckboxChange }) => (
   <div className={`form-check ${className}`}>
     <label className="label-wrapper">
       <input type="checkbox" name={label} checked={isSelected} onChange={onCheckboxChange} />
