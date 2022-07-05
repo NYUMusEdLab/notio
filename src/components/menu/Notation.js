ea4e403819e9165a9d8230b6c5e11b5a06d96bf3
saxjax <saxjax@saxjax.dk>
2022-07-05 16:30:13 +0200

    extended keyboard is broken,

--- src/components/menu/Notation.js
+++ src/components/menu/Notation.js
@@ -4,23 +4,23 @@ import React, { Component } from "react";
 import ListCheckbox from "../form/ListCheckbox";
 
 export const notations = [
-  "English",
-  "German",
-  "Romance",
-  "Relative",
-  "Scale Steps",
   "Chord extensions",
+  "Scale Steps",
+  "Relative",
+  "Romance",
+  "German",
+  "English",
 ];
 
 class Notation extends Component {
   render() {
     return (
       <div className="items-list">
-      <ListCheckbox
-        options={notations}
-        handleCheckboxChange={this.props.handleChange}
-        initOptions={this.props.initOptions}
-      />
+        <ListCheckbox
+          options={notations}
+          handleCheckboxChange={this.props.handleChange}
+          initOptions={this.props.initOptions}
+        />
       </div>
     );
   }
