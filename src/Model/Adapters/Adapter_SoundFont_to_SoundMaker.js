01fb0d6fa073c3217f92233c2014ed73c552c9d9
saxjax <saxjax@saxjax.dk>
2023-04-22 15:40:55 +0200

    tones now stop when button is released

--- src/Model/Adapters/Adapter_SoundFont_to_SoundMaker.js
+++ src/Model/Adapters/Adapter_SoundFont_to_SoundMaker.js
@@ -17,20 +17,24 @@ class Adapter_SoundFont_to_SoundMaker extends Adapter_X_to_SoundMaker {
   //   }
   constructor(props) {
     super(props);
+    this.soundfont = Soundfont;
     this.instrumentSound = props.instrumentSound;
     this.velocities = props.velocities;
+    this.ac = new AudioContext();
     this.synth = this.chooseInstrument();
+    this.playingNotes = {};
+    this.releaseNotes = [];
   }
   Instruments = SoundFontLibraryNames;
 
   chooseInstrument() {
-    if (this.Instruments.some((inst) => this.instrumentSound === inst.name)) {
-      return Soundfont.instrument(new AudioContext(), this.instrumentSound, {
+    if (this.Instruments.some((inst) => inst.name === this.instrumentSound)) {
+      return Soundfont.instrument(this.ac, this.instrumentSound, {
         soundfont: "FluidR3_GM",
         gain: 2.0,
       });
     } else {
-      return Soundfont.instrument(new AudioContext(), "acoustic_grand_piano", {
+      return Soundfont.instrument(this.ac, "acoustic_grand_piano", {
         soundfont: "FluidR3_GM",
         gain: 2.0,
       });
@@ -41,21 +45,28 @@ class Adapter_SoundFont_to_SoundMaker extends Adapter_X_to_SoundMaker {
   }
 
   getState(note) {
-    return true;
+    return this.ac.state;
   }
 
   resumeSound(tone) {}
 
   startSound(note) {
+    if (this.playingNotes.hasOwnProperty(note)) {
+      return;
+    }
     this.synth.then((instrument) => {
-      instrument.play(note);
+      this.playingNotes[note] = instrument.play(note);
     });
   }
 
   stopSound(note) {
-    this.synth.then((instrument) => {
-      instrument.stop(note);
-    });
+    if (this.playingNotes.hasOwnProperty(note)) {
+      // this.playingNotes.delete(note)
+      this.synth.then((instrument) => {
+        this.playingNotes[note].stop(this.ac.currentTime);
+        delete this.playingNotes[note];
+      });
+    }
   }
 
   render() {
