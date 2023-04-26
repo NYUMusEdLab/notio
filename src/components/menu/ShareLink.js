2d9c3ce3403df143e379467fa193af716884deda
saxjax <saxjax@saxjax.dk>
2023-04-27 00:19:42 +0200

    simplified html

--- src/components/menu/ShareLink.js
+++ src/components/menu/ShareLink.js
@@ -12,21 +12,35 @@ const ShareLink = (props) => {
     <div className="share-link">
       <h2>Share</h2>
       <p>Share your current setup:</p>
-      <button onClick={ async () => {
-        if(url === ""){
-          let dbId = await props.saveSessionToDB();
-          let url = "/shared/" + dbId;
-          setUrl(url);
-          setFullUrl(window.location.host + url);
-        }
-        copyToClipBoard(fullUrl);}}>Create Share Link</button>
-      <span className={`message ${url !== "" ? "" : "show"}`}>Store your current setup and share it with a link.</span>
-      <span className={`message ${url !== "" ? "show" : ""}`}>The link is copied to your clipboard and can be sent to others to open the same setup</span>
-      <a href={url} className={`message ${url !== "" ? "show" : ""}`} title="" target="_blank" rel="noopener noreferrer">
-          { fullUrl }
-        </a>
+      <a
+        href={url}
+        className={`message ${url !== "" ? "show" : ""}`}
+        title=""
+        target="_blank"
+        rel="noopener noreferrer">
+        {fullUrl}
+      </a>
+      <br></br>
+      <button
+        onClick={async () => {
+          if (url === "") {
+            let dbId = await props.saveSessionToDB();
+            let url = "/shared/" + dbId;
+            setUrl(url);
+            setFullUrl(window.location.host + url);
+          }
+          copyToClipBoard(fullUrl);
+        }}>
+        {url ? "Copy to clipboard" : "Create Share Link"}
+      </button>
+      <span className={`message ${url !== "" ? "" : "show"}`}>
+        Store your current setup and share it with a link.
+      </span>
+      <span className={`message ${url !== "" ? "show" : ""}`}>
+        The link is copied to your clipboard and can be sent to others to open the same setup
+      </span>
     </div>
-  )
+  );
 };
-  
+
 export default ShareLink;
