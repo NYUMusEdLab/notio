72e321b2108af1f23b8dcc37a80de64b7c1122b5
saxjax <saxjax@saxjax.dk>
2026-04-21 21:51:21 +0200

    Revert "Fix deterministic notation stacking order for issue #350"
    This reverts commit fb80ceb4f069f448c0c0a05ef2a99c72aec3c530.
    
    Reason: the prior fix edited the generic ListCheckbox component rather
    than the real ordering owner (WholeApp state + Keyboard/ColorKey render
    pipeline) and missed the session-restore path. Replanning per task file
    with Option 1 (state-boundary normalization in WholeApp).
    
    Conflict resolution: kept the current HEAD version of the issue-350
    task.md (it was rewritten on this branch with the new roadmap after
    fb80ceb was authored).

--- src/components/form/ListCheckbox.js
+++ src/components/form/ListCheckbox.js
@@ -1,50 +1,18 @@
 /* eslint-disable no-fallthrough */
 
 import React, { Component } from "react";
-import PropTypes from "prop-types";
 import Checkbox from "./Checkbox";
+import _ from "lodash";
 class ListCheckbox extends Component {
-  normalizeArray = (arr) => (Array.isArray(arr) ? arr : []);
-
-  arraysEqual = (a, b) => {
-    if (a.length !== b.length) {
-      return false;
-    }
-    return a.every((item, index) => item === b[index]);
-  };
-
-  getCanonicalOrder = (options = this.props.options, initOptions = this.props.initOptions) => {
-    const normalizedOptions = this.normalizeArray(options);
-    const normalizedInitOptions = this.normalizeArray(initOptions);
-    const extraSelected = normalizedInitOptions.filter(
-      (option) => !normalizedOptions.includes(option)
-    );
-    return [...extraSelected, ...normalizedOptions];
-  };
-
-  buildCheckboxState = (options = this.props.options, initOptions = this.props.initOptions) => {
-    const normalizedInitOptions = this.normalizeArray(initOptions);
-    const selected = new Set(normalizedInitOptions);
-    return this.getCanonicalOrder(options, initOptions).reduce((acc, option) => {
-      acc[option] = selected.has(option);
-      return acc;
-    }, {});
-  };
-
   state = {
-    checkboxes: this.buildCheckboxState(),
+    checkboxes: this.props.initOptions.reduce(
+      (options, option) => ({
+        ...options,
+        [option]: true,
+      }),
+      {}
+    ),
   };
-
-  componentDidUpdate(prevProps) {
-    const prevInit = this.normalizeArray(prevProps.initOptions);
-    const nextInit = this.normalizeArray(this.props.initOptions);
-    const prevOptions = this.normalizeArray(prevProps.options);
-    const nextOptions = this.normalizeArray(this.props.options);
-
-    if (!this.arraysEqual(prevInit, nextInit) || !this.arraysEqual(prevOptions, nextOptions)) {
-      this.setState({ checkboxes: this.buildCheckboxState(this.props.options, this.props.initOptions) });
-    }
-  }
 
   onChange = (changeEvent) => {
     const { name } = changeEvent.target;
@@ -56,10 +24,10 @@ class ListCheckbox extends Component {
         },
       }),
       () => {
-        const selectedOptions = this.getCanonicalOrder().filter(
-          (option) => this.state.checkboxes[option]
-        );
-        this.props.handleCheckboxChange(selectedOptions);
+        let arrOptions = _.pickBy(this.state.checkboxes, function (value, key) {
+          return value;
+        });
+        this.props.handleCheckboxChange(Object.keys(arrOptions));
       }
     );
   };
@@ -68,7 +36,7 @@ class ListCheckbox extends Component {
     <Checkbox
       className={`item--${option.replace("#", "sh")}`}
       label={option}
-      isSelected={Boolean(this.state.checkboxes[option])}
+      isSelected={this.state.checkboxes[option]}
       onCheckboxChange={this.onChange}
       key={option}
     />
@@ -77,18 +45,9 @@ class ListCheckbox extends Component {
   createCheckboxes = () => this.props.options.map(this.createCheckbox);
 
   render() {
+    // console.log("********** this.props.checked", this.props.checked);
     return this.createCheckboxes();
   }
 }
-
-ListCheckbox.propTypes = {
-  options: PropTypes.arrayOf(PropTypes.string).isRequired,
-  initOptions: PropTypes.arrayOf(PropTypes.string),
-  handleCheckboxChange: PropTypes.func.isRequired,
-};
-
-ListCheckbox.defaultProps = {
-  initOptions: [],
-};
 
 export default ListCheckbox;
